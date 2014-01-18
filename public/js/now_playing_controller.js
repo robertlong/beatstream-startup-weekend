f52e711e4cd22d92c2e96e70e2040b83c26936d1
Matt Bleifer <mattbleifer@gmail.com>
2014-01-18 15:11:01 -0800

    Updated medie bar components

--- public/js/now_playing_controller.js
+++ public/js/now_playing_controller.js
@@ -1,10 +1,42 @@
 function NowPlayingCtrl ($scope) {
 
+	$scope.song = {
+		icon: "",
+		name: "",
+		artist: ""
+	};
+
+	$('#rdio-api').bind('playingTrackChanged.rdio', function(e, playingTrack, sourcePosition) {
+		$scope.song = playingTrack;
+		$scope.$apply();
+	})
+
   // Docs availible here: http://www.eyecon.ro/bootstrap-slider/
-  $('.volume-slider').slider();
+  $('.volume-slider').slider({
+  	max: 100,
+  	value: 50,
+  	slide: function(event, ui) {
+  		var volume = ui.value;
+  		console.log(volume);
+  		Rdio.setVolume(volume/100);
+  	}
+  });
 
   $scope.play = function() {
-    Rdio.play('a171827');
+    Rdio.play('a171827', {
+    	initialPosition: 5
+    });
   };
 
+  $scope.pause = function() {
+  	Rdio.pause('a171827');
+  };
+
+  $scope.next = function() {
+  	Rdio.next();
+  };
+
+  $scope.previous = function() {
+  	Rdio.previous();
+  };
 }
