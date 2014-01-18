c2c1289a98e344820b85721a57bbe66ea788afa8
Robert <robert@robertlong.me>
2014-01-18 11:41:11 -0800

    Added rdio initialization.

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,8 @@
 var App = {
 
 };
+
+$(document).ready(function() {
+  $('#rdio-api').rdio('GAlNi78J_____zlyYWs5ZG02N2pkaHlhcWsyOWJtYjkyN2xvY2FsaG9zdEbwl7EHvbylWSWFWYMZwfc=');
+  window.Rdio = $("#rdio-api").rdio();
+});
--- public/js/now_playing_controller.js
+++ public/js/now_playing_controller.js
@@ -3,4 +3,8 @@ function NowPlayingCtrl ($scope) {
   // Docs availible here: http://www.eyecon.ro/bootstrap-slider/
   $('.volume-slider').slider();
 
+  $scope.play = function() {
+    Rdio.play('a171827');
+  };
+
 }
