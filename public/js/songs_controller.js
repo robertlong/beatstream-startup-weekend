22a7baef4011223839b8b63177ae5072bbea211d
Robert <robert@robertlong.me>
2014-01-19 15:19:17 -0800

    Fixed play song index.

--- public/js/songs_controller.js
+++ public/js/songs_controller.js
@@ -1,7 +1,8 @@
 function SongsCtrl ($scope, $firebase) {
   $scope.songs = $firebase(FirebaseRef.child('posts'));
 
-  $scope.play = function(songId) {
+  $scope.play = function(songId, index) {
+    SongState.songIndex = index;
     $("#rdio-api").rdio().play(songId);
   };
 }
